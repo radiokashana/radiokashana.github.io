62bc60d5ad1945948f2a74d985a9606d3d23d5c2
Jorge Vargas <yorch.veintemil+github@gmail.com>
2018-03-04 09:29:43 -0800

    deps: add tachyons

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,6 +4,7 @@ import Helmet from 'react-helmet'
 
 import Header from '../components/Header'
 import './index.css'
+import 'tachyons/css/tachyons.css'
 
 const TemplateWrapper = ({ children }) => (
   <div>
