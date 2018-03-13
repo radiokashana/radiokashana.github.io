4c285a0b5ce8f8cc28575729997ec28d08fbb639
Jorge Vargas <yorch.veintemil+github@gmail.com>
2018-03-13 12:23:33 -0700

    change footer links

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,13 +6,11 @@ export default () =>
 	<footer className="white w-100 tc bg-near-black cl">
 		<nav>
 			<ul className="dib list ma0 pa0 f6">
-				<FooterMenuItem href="#">Zoom 95</FooterMenuItem>
-				<FooterMenuItem href="#">Asociaci&oacute;n Mundial de Radios Comunitarias AMARC</FooterMenuItem>
-				<FooterMenuItem href="#">AMARC Am&eacute;rica Latina y el Caribe</FooterMenuItem>
-				<FooterMenuItem href="#">AMARC M&eacute;xico</FooterMenuItem>
-				<FooterMenuItem href="#">&iquest;Qu&eacute; es una radio comunitaria?</FooterMenuItem>
+				<FooterMenuItem href="http://www.amarcmexico.org/">AMARC M&eacute;xico</FooterMenuItem>
+				<FooterMenuItem href="https://www.facebook.com/radiokashana/">Facebook Radiokashana</FooterMenuItem>
+				<FooterMenuItem href="https://www.facebook.com/lavozdelpacificoradio/">Facebook La Voz del Pac&iacute;fico</FooterMenuItem>
 			</ul>
 		</nav>
-		<h3 className="ma0">Santa Rosal&iacute;a, Baja California Sur.</h3>
+		<h3 className="ma0">Transmitiendo en la 93.3 FM desde el punto m&aacute;s alto de Santa Rosal&iacute;a, Baja California Sur</h3>
 		<h3 className="ma0">Cel. 615 155 8484 rafael@radiokashana.org</h3>
 	</footer>
